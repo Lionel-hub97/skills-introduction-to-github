f01a4126a8099dcd923049915066ecf4851646c6
albc9 <abordach108@ub.edu.cat>
2024-10-07 13:11:22 +0200

    Añadí la practica 0

--- src/Taximetro.java
+++ src/Taximetro.java
@@ -1,2 +1,31 @@
+/* taula de tests
+  entrada            | sortida esperada
+  -------------------------------------
+   zona_a + (minuts)  | precio
+   zona_b + (minuts)  | precio
+   zona_c +   (km)    | precio
+*/
+
+
+inport java.util.*;
 public class Taximetro {
+    public static void main(String[] args) {
+        int day = 3; // Puedes cambiar el valor para probar otros casos.
+
+
+        switch (zona){
+            case 1:
+                System.out.println("ZONA_A")
+                break;
+            case 2:
+                System.out.println("ZONA_B")
+                break;
+            case 3:
+                System.out.println("ZONA_C")
+                break;
+            default:
+                System.out.println("NO HAY ZONAS PARA TU PRESENCIA")
+                break;
+        }
+    }
 }
