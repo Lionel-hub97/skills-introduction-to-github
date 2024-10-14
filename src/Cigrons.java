ec9fe86bdc8e5ff53a2ddd35553d1dbbc9d93180
albc9 <abordach108@ub.edu.cat>
2024-10-14 19:32:34 +0200

    Añadí la practica 0

---
+++ src/Cigrons.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Cigrons {
+    public static void main(String[] args) {
+        int n, m, kg, bosGrans = 0, bosPetit = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter bosses petites:");
+        n = sc.nextInt();
+        System.out.println("Enter bosses grans:");
+        m = sc.nextInt();
+        System.out.println("Enter kg:");
+        kg = sc.nextInt();
+
+        while (kg != 0){
+            if (kg % 5 == 0){
+                for (int i = 0; i < m; i++){
+                    bosGrans = kg - 5;
+                }
+            }else{
+                for (int i = 0; i < n; i++){
+                    bosPetit = kg - 1;
+                }
+            }
+        }
+        System.out.println(bosPetit);
+    }
+}
---
+++ src/NoFibonacci.java
@@ -0,0 +1,15 @@
+public class NoFibonacci {
+    public static void main(String[] args) {
+
+        int x = 1, y = 0, z;
+        for(int i = 0; i < 45; i++){
+            z = x + y;
+            x = y;
+            y = z;
+            for(int k = x + 1; k < y && k <= 45; k++){
+                System.out.println(k);
+            }
+        }
+        System.out.println("end");
+    }
+}
