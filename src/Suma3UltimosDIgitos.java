b38e422f0b3e42b6df3b54036dafd83f1694663e
albc9 <abordach108@ub.edu.cat>
2024-10-21 13:26:37 +0200

    Añadí la practica 0

---
+++ src/Suma3UltimosDIgitos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Suma3UltimosDIgitos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el nombre: ");
+        int nombre = sc.nextInt();
+        int valor = 0;
+        int suma = 0;
+        for (int i = 0; i < 3; i++) {
+            int rest = nombre % 10;
+            nombre -= rest;
+            suma += rest;
+            nombre = nombre/10;
+        }
+        System.out.println(suma);
+
+    }
+}
