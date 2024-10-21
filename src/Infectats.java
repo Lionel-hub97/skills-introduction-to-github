d13d59ad3e759bfa4f6ffa40f8a89d395878c0bf
albc9 <abordach108@ub.edu.cat>
2024-10-21 18:59:15 +0200

    Añadí la practica 0

---
+++ src/Infectats.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Infectats {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nombre de persones: ");
+        int nombre = sc.nextInt(), min = nombre, max = 1;
+
+        for (int i = 1; i <= nombre; i++) {
+
+        }
+
+
+    }
+}
